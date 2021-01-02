3648411f15712fe412762356e050927c7e12a7c9
9haomi <>
2021-01-02 23:10:50 +0800

    更新模型

--- SwiftUIAll/JBBHomeView/BaseListView.swift
+++ SwiftUIAll/JBBHomeView/BaseListView.swift
@@ -11,6 +11,18 @@ struct BaseListView: View {
     var body: some View {
         List {
             
+            Section() {
+                WatchRingsView()
+//                ScrollView(.horizontal, showsIndicators: false) {
+//                    WatchRingsView()
+//                         //.padding(.horizontal, 30)
+//                        //.padding(.bottom, 5)
+//                        .onTapGesture {
+//                            //可加点击事件
+//                        }
+//                }
+            }
+            
             Section(header: Text("文本").foregroundColor(Color.blue)) {
                 TaskRow(title:"Text" ,desdetail:"显示一行或者多行文本控件")
                 TaskRow(title:"TextField" ,desdetail:"显示编辑文本的输入控件")
@@ -36,39 +48,9 @@ struct BaseListView: View {
                 TaskRow(title:"Slider" ,desdetail:"滑动设置指定范围的值")
                 TaskRow(title:"Stepper" ,desdetail:"可以增加或减少数值")
             }
-//
-//            Section(header: Text("Other tasks")) {
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//            }
-//
-//            Section(header: Text("Other tasks")) {
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//            }
-//
-//            Section(header: Text("Other tasks")) {
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//            }
-//
-//            Section(header: Text("Other tasks")) {
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//                TaskRow(title:"巨形")
-//            }
-//
-//            Section(header: Text("常用形状")) {
-//                TaskRow(title:"矩形")
-//                TaskRow(title:"圆角矩形")
-//                TaskRow(title:"圆形")
-//                TaskRow(title:"椭圆形")
-//                TaskRow(title:"胶囊")
-//            }
+
         }
+        
     }
 }
 
@@ -95,34 +77,109 @@ struct TaskRowDetail: View {
     var title: String
     
     var body: some View {
-        switch title {
-        case "矩形":
-            Text("矩形")
-            Rectangle() //矩形
-                .frame(width: 200, height: 50)
-        case "圆角矩形":
-            Text("圆角矩形")
-            RoundedRectangle(cornerRadius: 10, style: .continuous) //圆角矩形
-                .frame(width: 200, height: 50)
-        case "圆形":
-            Text("圆形")
-            Circle() //圆形
-                .frame(width: 50, height: 50)
-        case "椭圆形":
-            Text("椭圆形")
-            Ellipse() //椭圆形
-                .frame(width: 200, height: 50)
-        case "胶囊":
-            Text("胶囊")
-            Capsule() //胶囊
-                .frame(width: 200, height: 50)
-        default:
-            Text("default")
+        NavigationView {
+            switch title {
+            case "矩形":
+                Text("矩形")
+                Rectangle() //矩形
+                    .frame(width: 200, height: 50)
+            case "圆角矩形":
+                Text("圆角矩形")
+                RoundedRectangle(cornerRadius: 10, style: .continuous) //圆角矩形
+                    .frame(width: 200, height: 50)
+            case "圆形":
+                Text("圆形")
+                Circle() //圆形
+                    .frame(width: 50, height: 50)
+            case "椭圆形":
+                Text("椭圆形")
+                Ellipse() //椭圆形
+                    .frame(width: 200, height: 50)
+            case "胶囊":
+                Text("胶囊")
+                Capsule() //胶囊
+                    .frame(width: 200, height: 50)
+            default:
+                Text("default")
+            }
         }
-        
+ 
     }
 }
 
+struct WatchRingsView: View {
+    
+    var dayYear = 0
+    var dayPercentageYear = 0.0
+    var rightRunText:String = "平年"
+    
+    func dayOfYear(_ date: String) -> Int {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let endDate = dateFormatter.date(from: date)!
+        let year = date.components(separatedBy: "-").first!
+        let startStr = year + "-01-01"
+        let startDate = dateFormatter.date(from: startStr)!
+        
+        let days = Calendar.current.dateComponents([.day], from: startDate, to: endDate).day!
+        return days + 1
+    }
+    
+    func isRun(_ date: String) -> Bool {
+        let dateArray = date.split(separator: "-")
+        let y = Int(dateArray[0])!
+        let isRun = ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)
+        if isRun {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    init(){
+        let  formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        let dates = formatter.string(from: Date())
+        
+        dayYear = self.dayOfYear(dates)
+        
+        let isR = self.isRun(dates)
+        
+        if isR {
+            rightRunText = "闰年"
+            dayPercentageYear = 100-Double(dayYear)/366.0
+        }else{
+            rightRunText = "平年"
+            dayPercentageYear = 100-Double(dayYear)/365.0
+        }
+    }
+    
+    var body: some View {
+ 
+        HStack(spacing: 50) {
+            HStack(spacing: 12.0) {
+                RingView(color1: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 50, height: 50, percent: CGFloat(dayPercentageYear), show: .constant(true))
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Time countdown").bold().modifier(FontModifier(style: .footnote))
+                    Text("Today is the \(dayYear)th day of the year").modifier(FontModifier(style: .caption))
+                }
+                .modifier(FontModifier())
+            }
+            .padding(8)
+            .background(Color("background3"))
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+            
+            Text(self.rightRunText)
+                .foregroundColor(.blue)
+                .bold()
+                .font(.system(.headline))
+
+        }
+    }
+    
+}
 struct BaseListView_Previews: PreviewProvider {
     static var previews: some View {
         BaseListView()
